056caf28250cf487c20e3aff65dbd109d20f44f3
Devon Wells <devon.wellsa@gmail.com>
2021-11-17 22:14:34 -0500

    Basic quote form ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,9 @@ import {
   FormLabel,
   FormErrorMessage,
   FormHelperText,
+  Box,
+  Flex,
+  Stack,
   Input,
   Button,
   Fade,
@@ -25,7 +28,96 @@ import { collection } from "firebase/firestore";
 import { db } from "../utils/firebase/clientApp";
 
 import axios from "axios";
-import { IUser } from "../types";
+import { IUser, QuotationRequest } from "../types";
+
+const QuoteForm = () => {
+  const handleFetchQuote = async (params: QuotationRequest) => {
+    try {
+      const res = await axios.post("/api/quotation", {
+        ...params,
+      });
+      console.log(res.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <Box my="2rem">
+      <Formik
+        initialValues={{
+          age: 28,
+          currency_id: "",
+          start_date: "",
+          end_date: "",
+        }}
+        onSubmit={(params, { setSubmitting }) => {
+          handleFetchQuote(params);
+          setSubmitting(false);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <Stack spacing={4}>
+              <Field name="age" placeholder="Age">
+                {({ field }) => (
+                  <FormControl id="age">
+                    <FormLabel>Age</FormLabel>
+                    <Input {...field} id="age" />
+                    <FormHelperText>
+                      It's free if you're under 18.
+                    </FormHelperText>
+                  </FormControl>
+                )}
+              </Field>
+              <Field name="currency_id" placeholder="Currency">
+                {({ field }) => (
+                  <FormControl id="currency_id">
+                    <FormLabel>Currency</FormLabel>
+                    <Input {...field} id="currency_id" />
+                    <FormHelperText>
+                      Choose the currency you want to use.
+                    </FormHelperText>
+                  </FormControl>
+                )}
+              </Field>
+              <Field name="start_date" placeholder="Start Date">
+                {({ field }) => (
+                  <FormControl id="start_date">
+                    <FormLabel>Start Date</FormLabel>
+                    <Input {...field} id="start_date" />
+                    <FormHelperText>
+                      Choose the start date of your trip (YYYY-MM-DD).
+                    </FormHelperText>
+                  </FormControl>
+                )}
+              </Field>
+              <Field name="end_date" placeholder="End Date">
+                {({ field }) => (
+                  <FormControl id="end_date">
+                    <FormLabel>End Date</FormLabel>
+                    <Input {...field} id="end_date" />
+                    <FormHelperText>
+                      Choose the end date of your trip (YYYY-MM-DD).
+                    </FormHelperText>
+                  </FormControl>
+                )}
+              </Field>              
+            </Stack>
+            <Button
+              isLoading={isSubmitting}
+              mt={4}
+              colorScheme="teal"
+              type="submit"
+            >
+              Quote Me!
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Box>
+  );
+};
 
 const Home = () => {
   const [user, setUser] = useState<IUser | null>(null);
@@ -65,53 +157,58 @@ const Home = () => {
       <Hero />
       <Main>
         {user ? (
-          <>
-            <Fade in={!!user}>
-              <Text fontSize="xl" fontWeight="bold" color="gray.500">
-                Welcome back, {user.username}!
-              </Text>
+          <Flex flexDirection="column" justifyContent="space-between">
+            <Box>
+              <Fade in={!!user}>
+                <Text fontSize="xl" fontWeight="bold" color="gray.500">
+                  Welcome, {user.username}!
+                </Text>
 
-              <Text fontSize="lg" color="gray.500">
-                You are now logged in.
-              </Text>
-            </Fade>
-          </>
+                <Text fontSize="lg" color="gray.500">
+                  You are now logged in. Let's get you a quote.
+                </Text>
+              </Fade>
+            </Box>
+            <QuoteForm />
+          </Flex>
         ) : (
-          <Text fontSize="xl" fontWeight="bold" color="gray.500">
-            Please log in to get started.
-          </Text>
+          <>
+            <Text fontSize="xl" fontWeight="bold" color="gray.500">
+              Please log in to get started.
+            </Text>
+            <Formik
+              initialValues={{ username: "" }}
+              onSubmit={(values, { setSubmitting }) => {
+                handleLogin(values.username);
+                setSubmitting(false);
+              }}
+            >
+              {({ isSubmitting }) => (
+                <Form>
+                  <Field name="username" placeholder="Username">
+                    {({ field }) => (
+                      <FormControl id="username">
+                        <FormLabel>Username</FormLabel>
+                        <Input {...field} id="username" />
+                        <FormHelperText>
+                          Any username will work for now!
+                        </FormHelperText>
+                      </FormControl>
+                    )}
+                  </Field>
+                  <Button
+                    isLoading={isSubmitting}
+                    mt={4}
+                    colorScheme="teal"
+                    type="submit"
+                  >
+                    Login
+                  </Button>
+                </Form>
+              )}
+            </Formik>
+          </>
         )}
-        <Formik
-          initialValues={{ username: "" }}
-          onSubmit={(values, { setSubmitting }) => {
-            handleLogin(values.username);
-            setSubmitting(false);
-          }}
-        >
-          {({ isSubmitting }) => (
-            <Form>
-              <Field name="username" placeholder="Username">
-                {({ field }) => (
-                  <FormControl id="username">
-                    <FormLabel>Username</FormLabel>
-                    <Input {...field} id="username" />
-                    <FormHelperText>
-                      Any username will work for now!
-                    </FormHelperText>
-                  </FormControl>
-                )}
-              </Field>
-              <Button
-                isLoading={isSubmitting}
-                mt={4}
-                colorScheme="teal"
-                type="submit"
-              >
-                Login
-              </Button>
-            </Form>
-          )}
-        </Formik>
       </Main>
 
       <DarkModeSwitch />
